f6b8b25dfae28a86fd2ae388b3448cb5c627fef6
jAlaneAmilon <202480052@psu.palawan.edu.ph>
2025-08-14 12:35:00 +0800

    Update practice0.1/0.2.cs

--- squares.py
+++ squares.py
@@ -1,6 +1,40 @@
+using System;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Practice 01.1*/
+        Console.WriteLine("Enter number to check if even:");
 
-squares_comp = []
+        double a = Convert.ToDouble(Console.ReadLine());
 
-for x in range (1, 11):
-    squares_comp.append(x)
-    print(len(squares_comp)**2)
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("The number " + a + " is even");
+        }
+        else
+        {
+            Console.WriteLine("The number " + a + " is not even");
+        }
+        /*Practice 01.2:*/
+
+        Console.WriteLine("Enter a number to check if negative or positive");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num < 0)
+        {
+            Console.WriteLine("The number " + num + " is negative");
+        }
+        else if (num > 0)
+        {
+            Console.WriteLine("The number " + num + " is positive");
+        }
+        else
+        {
+            Console.WriteLine("The number " + num + " is neither positive or negative");
+        }
+
+    }
+
+        
+    }
