7fa4e80c0b101d0053be9e3556bdb4b6882f8085
jAlaneAmilon <202480052@psu.palawan.edu.ph>
2025-09-11 13:11:18 +0800

    Drill1.c

--- Drill1.c
+++ Drill1.c
@@ -1,49 +1,42 @@
 //Create a C program that asks for 5 positive integer. Get the sum and product of even numbers. Provide error if no even numbers provided.
-
 #include <stdio.h>
 
-int main() {
-    #include <stdio.h>
+int main() {   
+    int holder[5];
+    int sum = 0; 
+    int product = 1;
+    int CountOfEven = 0;
 
-int main() {
-    int numbers[5];
-    int sum = 0, product = 1;
-    int even_count = 0;
 
-    // Get 5 positive integers from the user
     printf("Enter 5 positive integers:\n");
     for (int i = 0; i < 5; i++) {
         printf("Number %d: ", i + 1);
-        scanf("%d", &numbers[i]);
+        scanf("%d", &holder[i]);
 
-        // Ensure the number is positive
-        if (numbers[i] <= 0) {
+    
+        if (holder[i] <= 0) {
             printf("Please enter a positive integer.\n");
-            i--; // Retry this number
+            i--;
             continue;
         }
     }
 
-    // Process even numbers
+
     for (int i = 0; i < 5; i++) {
-        if (numbers[i] % 2 == 0) {
-            sum += numbers[i];
-            product *= numbers[i];
-            even_count++;
+        if (holder[i] % 2 == 0) {
+            sum += holder[i];
+            product *= holder[i];
+            CountOfEven++;
         }
     }
 
-    // If no even numbers were entered
-    if (even_count == 0) {
+
+    if (CountOfEven == 0) {
         printf("Error: No even numbers entered.\n");
     } else {
         printf("Sum of even numbers: %d\n", sum);
         printf("Product of even numbers: %d\n", product);
     }
-
-    return 0;
-}
-
     
     return 0;
 }
